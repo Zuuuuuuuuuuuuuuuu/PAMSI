0ba0bd4940d301934387c14cc89067ee2820faf8
Zuuuuuuuuuuuuuuuu <zuzaxmejer@gmail.com>
2022-04-27 20:48:45 +0200

    porządki

---
+++ 2_projekt/program/build/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/src/driver.cpp" "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/build/CMakeFiles/a.out.dir/src/driver.cpp.o"
+  "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/src/files.cpp" "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/build/CMakeFiles/a.out.dir/src/files.cpp.o"
+  "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/src/merge_sort.cpp" "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/build/CMakeFiles/a.out.dir/src/merge_sort.cpp.o"
+  "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/src/quick_sort.cpp" "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/build/CMakeFiles/a.out.dir/src/quick_sort.cpp.o"
+  "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/src/vector.cpp" "/home/zuzanna/Pulpit/sem4/PAMSI/2_projekt/program/build/CMakeFiles/a.out.dir/src/vector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 2_projekt/program/build/CMakeFiles/a.out.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.out.dir/src/driver.cpp.o"
+  "CMakeFiles/a.out.dir/src/files.cpp.o"
+  "CMakeFiles/a.out.dir/src/merge_sort.cpp.o"
+  "CMakeFiles/a.out.dir/src/quick_sort.cpp.o"
+  "CMakeFiles/a.out.dir/src/vector.cpp.o"
+  "a.out"
+  "a.out.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a.out.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
